6c9f0751a0ab9f0d8e92e42b94875464ae1261dc
krgadde <karuna.gadde@gmail.com>
2021-06-20 06:38:41 -0500

    Adding krgadde repo

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,29 @@
+import json
 from flask import Flask
+from flask import Response
+import logging
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info("Main request successfull")
     return "Hello World!"
 
+@app.route("/status")
+def httpstatus():
+    response= app.response_class(response=json.dumps({'result':'OK - healthy'}),status=200,mimetype='application/json')
+    app.logger.info("Status request successfull")
+    return response
+    #status_code = flask.Response(status=200)
+    #return status_code
+    #return Response("{'result':'OK - healthy'}", status=status_code, mimetype='application/json')
+
+
+@app.route("/metrics")
+def appmetrics():
+    app.logger.info("Metrics request successfull")
+    return Response("{'UserCount':'140','UserCountActive':'23'}", status=200, mimetype='application/json')
+
 if __name__ == "__main__":
+    logging.basicConfig(filename="app.log",level=logging.DEBUG)
     app.run(host='0.0.0.0')
